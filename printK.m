e4d98d4c129b0273ceb098edb9a23a9e1ad5b8a5
hombourger_b <boris.hombourger@psi.ch>
2017-12-28 20:30:34 +0100

    removed useless files

--- printK.m
+++ printK.m
@@ -1,5 +1,5 @@
 function [ ] = printK(SYS, step, prefix, source)
-%PRINTK Prints K-eff values to keff.txt
+%PRINTK=(SYS,step,prefix,source) Prints K-eff values to keff.txt
 
 fprintf(SYS.FID.keff,'%-7s %-3s %5.3d %4.3d %3s %-12.6G %-9f %-9f\n',...
     source,prefix,SYS.ouCntr,SYS.inCntr,step,SYS.nowTime(end),SYS.kinf(end),SYS.keff(end));
